0e0c3e79329615130fd6dce3ec90445729a715fb
upendraupi <upendrakuruma@gmail.com>
2024-08-09 17:33:18 +0530

    thirdcommit

--- contactapi/settings.py
+++ contactapi/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = "contactapi.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR,'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -137,8 +137,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- contactapi/urls.py
+++ contactapi/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path,include
 from django.urls import re_path
 from rest_framework import permissions
+from django.conf import settings
+from django.conf.urls.static import static
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 schema_view = get_schema_view(
@@ -41,3 +43,7 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
+
+
+urlpatterns +=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
